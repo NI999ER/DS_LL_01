e7d8820dfc2609b4f04dcdfed1ce3d6c907ff903
Daniels Posers <145805019+NI999ER@users.noreply.github.com>
2025-02-12 11:30:02 +0000

    241RDB021

--- LL_1.py
+++ LL_1.py
@@ -39,6 +39,6 @@ my_linked_list.append(2)
 my_linked_list.append(3)
 my_linked_list.append(4)
 my_linked_list.append(5)
-
+my_linked_list.append(6)
 print( my_linked_list.find_middle_node().value )
 
--- LL_2.py
+++ LL_2.py
@@ -41,6 +41,7 @@ my_linked_list_1 = LinkedList(1)
 my_linked_list_1.append(2)
 my_linked_list_1.append(3)
 my_linked_list_1.append(4)
+my_linked_list_1.append(5)
 my_linked_list_1.tail.next = my_linked_list_1.head
 print(my_linked_list_1.has_loop() ) # Returns True
 
@@ -51,6 +52,7 @@ my_linked_list_2 = LinkedList(1)
 my_linked_list_2.append(2)
 my_linked_list_2.append(3)
 my_linked_list_2.append(4)
+my_linked_list_2.append(5)
 print(my_linked_list_2.has_loop() ) # Returns False
 
 
